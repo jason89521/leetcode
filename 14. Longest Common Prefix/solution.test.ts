50e28da9f93bda0980d77bb8209f5583af65699a
Xuan <97ssps30212@gmail.com>
2022-04-04 14:42:06 +0800

    move to easy folder

--- 14. Longest Common Prefix/solution.test.ts
+++
@@ -1,9 +0,0 @@
-import solution from './solution'
-
-test('should return fl', () => {
-  expect(solution(['flower', 'flow', 'flight'])).toBe('fl');
-});
-
-test('should return empty string', () => {
-  expect(solution(['dog', 'racecar', 'car'])).toBe('');
-});
